3f888c57667fa1a33282ced9eafb85a8a6cba4b1
Jack Gallagher <339690067@GoTVDSB.ca>
2024-10-02 09:36:17 -0400

    Finished

--- Topic 7 - Lists Assignment/Program.cs
+++ Topic 7 - Lists Assignment/Program.cs
@@ -7,12 +7,14 @@ namespace Topic_7___Lists_Assignment
         static void Main(string[] args)
         {
             List<int> numList = new List<int>();
+            List<int> numModeList = new List<int>();
+            List<string> vegetables = new List<string>() {"CARROT", "BEET", "CELERY", "RADISH", "CABBAGE" };
 
             Random generator = new Random();
 
-            string userChoice;
-            int numRemoveChoice, numAddChoice, numOccurrenceChoice, numOccurrenceCount, numMode, numMaxCount, numCurrentCount;
-            bool done = false;
+            string userChoice, vegetableRemove, vegetableSearch, vegetableAdd;
+            int numRemoveChoice, numAddChoice, numOccurrenceChoice, numOccurrenceCount, numMaxCount, numCurrentCount, indexRemove;
+            bool done = false, sameAsList = false;
 
             Console.WriteLine("Generating a list of numbers...");
             for (int i = 1; i <= 25; i++)
@@ -136,7 +138,7 @@ namespace Topic_7___Lists_Assignment
 
                     Console.Clear();
                 }
-                else if(userChoice == "5" || userChoice.ToLower() == "add number")
+                else if (userChoice == "5" || userChoice.ToLower() == "add number")
                 {
                     Console.WriteLine("Pick a number do you want to add to the list: ");
                     while (!Int32.TryParse(Console.ReadLine(), out numAddChoice))
@@ -149,7 +151,7 @@ namespace Topic_7___Lists_Assignment
                     Console.WriteLine("Printing the list...");
                     for (int i = 0; i <= numList.Count() - 1; i++)
                     {
-                            Console.Write(numList[i] + ", ");
+                        Console.Write(numList[i] + ", ");
                     }
 
                     Console.WriteLine();
@@ -275,12 +277,11 @@ namespace Topic_7___Lists_Assignment
                 else if (userChoice == "11" || userChoice.ToLower() == "most frequent value")
                 {
                     numList.Sort();
-                    numMode = 0;
                     numMaxCount = 0;
-                    numCurrentCount = 0;
+                    numCurrentCount = 1;
 
                     Console.WriteLine("Finding the most frequent value...");
-                    for (int i = 0; i <= numList.Count() - 1; i++)
+                    for (int i = 1; i <= numList.Count() - 1; i++)
                     {
                         if (numList[i] == numList[i - 1])
                         {
@@ -291,21 +292,230 @@ namespace Topic_7___Lists_Assignment
                             if (numCurrentCount > numMaxCount)
                             {
                                 numMaxCount = numCurrentCount;
-                                numMode = numList[i - 1];
+                                numModeList.Clear();
+                                numModeList.Add(numList[i - 1]);
                             }
-                            numCurrentCount = 0;
+                            else if (numCurrentCount == numMaxCount)
+                            {
+                                numModeList.Add(numList[i - 1]);
+                            }
+                            numCurrentCount = 1;
                         }
                     }
 
                     Console.WriteLine();
 
-                    Console.WriteLine("The mode is " + numMode + "!");
+                    Console.Write("The mode is ");
+                    for (int i = 0; i <= numModeList.Count() - 1; i++)
+                    {
+                        Console.Write(numModeList[i] + ", ");
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine();
 
                     Console.WriteLine("Printing the list...");
                     for (int i = 0; i <= numList.Count() - 1; i++)
                     {
                         Console.Write(numList[i] + ", ");
                     }
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Press ENTER to continue");
+                    Console.ReadLine();
+
+                    Console.Clear();
+                }
+                else if (userChoice == "12" || userChoice.ToLower() == "quit")
+                {
+                    Console.WriteLine("Quitting the program now...");
+
+                    Thread.Sleep(500);
+
+                    Console.Clear();
+
+                    done = true;
+                }
+                else
+                {
+                    Console.WriteLine("That's not an option, please enter a valid option");
+
+                    Console.WriteLine();
+
+                    Console.WriteLine("Press ENTER to continue");
+                    Console.ReadLine();
+
+                    Console.Clear();
+                }
+            }
+            done = false;
+
+            while (!done)
+            {
+                Console.WriteLine("Here is a list of vegetables:");
+
+                Console.WriteLine();
+
+                for (int i = 0; i <= vegetables.Count() - 1; i++)
+                {
+                    Console.WriteLine((i + 1) + ". " + vegetables[i]);
+                }
+
+                Console.WriteLine();
+
+                Console.WriteLine("What do you want to do:");
+
+                Console.WriteLine();
+
+                Console.WriteLine("1. Remove a vegetable by index");
+                Console.WriteLine("2. Remove a vegetable by value");
+                Console.WriteLine("3. Search for a vegetable");
+                Console.WriteLine("4. Add a vegetable");
+                Console.WriteLine("5. Sort the list");
+                Console.WriteLine("6. Clear the list");
+                Console.WriteLine("7. Quit");
+                userChoice = Console.ReadLine();
+
+                Console.WriteLine();
+
+                if (userChoice == "1" || userChoice.ToLower() == "remove vegetable by index")
+                {
+                    Console.WriteLine("Please enter the index of the vegetable you want to remove: ");
+                    while (!Int32.TryParse(Console.ReadLine(), out indexRemove))
+                        Console.WriteLine("That is not a valid number, please try again");
+
+                    Console.WriteLine();
+
+                    if (indexRemove > (vegetables.Count() - 1))
+                    {
+                        Console.WriteLine("Cannot remove item from list as the index value you gave is bigger than the list...");
+                    }
+                    else if (indexRemove < 0)
+                    {
+                        Console.WriteLine("Cannot remove item from list as the index value you gave is less than zero...");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Successfully removed " + vegetables[indexRemove] + " from the list!");
+
+                        vegetables.RemoveAt(indexRemove);
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Press ENTER to continue");
+                    Console.ReadLine();
+
+                    Console.Clear();
+                }
+                else if (userChoice == "2" || userChoice.ToLower() == "remove vegetable by value")
+                {
+                    Console.WriteLine("Pick an item from the list to remove:");
+                    vegetableRemove = Console.ReadLine();
+
+                    Console.WriteLine();
+
+                    if (vegetables.Remove(vegetableRemove))
+                    {
+                        Console.WriteLine("Successfully removed " + vegetableRemove.ToUpper() + " from the list!");
+                    }
+                    else if (!vegetables.Remove(vegetableRemove))
+                    {
+                        Console.WriteLine("Failed to remove " + vegetableRemove.ToUpper() + " from the list as it isn't part of the list");
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Press ENTER to continue");
+                    Console.ReadLine();
+
+                    Console.Clear();
+                }
+                else if (userChoice == "3" || userChoice.ToLower() == "search for vegetable")
+                {
+                    Console.WriteLine("What vegetable do you want to search for in the list: ");
+                    vegetableSearch = Console.ReadLine();
+
+                    Console.WriteLine();
+
+                    for (int i = 0; i <= vegetables.Count() - 1; i++)
+                    {
+                        if (vegetableSearch == vegetables[i])
+                        {
+                            Console.WriteLine("The index of the vegetable " + vegetables[i] + " is " + i + "!");
+                        }
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Press ENTER to continue");
+                    Console.ReadLine();
+
+                    Console.Clear();
+                }
+                else if (userChoice == "4" || userChoice.ToLower() == "add vegetable")
+                {
+                    Console.WriteLine("What vegetable do you want to add to the list: ");
+                    vegetableAdd = Console.ReadLine();
+
+                    Console.WriteLine();
+
+                    vegetableAdd = vegetableAdd.ToUpper();
+
+                    sameAsList = false;
+
+                    for (int i = 0; i <= vegetables.Count() - 1; i++)
+                    {
+                        if (vegetableAdd == vegetables[i])
+                        {
+                            Console.WriteLine("You cannot add a vegetable that's already in the list...");
+                            sameAsList = true;
+                        }
+                    }
+                    if (!sameAsList)
+                    {
+                        Console.WriteLine("Successfully added " + vegetableAdd + " to the list!");
+
+                        vegetables.Add(vegetableAdd);
+                    }
+                    Console.WriteLine();
+
+                    Console.WriteLine("Press ENTER to continue");
+                    Console.ReadLine();
+
+                    Console.Clear();
+                }
+                else if (userChoice == "5" || userChoice.ToLower() == "sort list")
+                {
+                    vegetables.Sort();
+
+                    Console.WriteLine("List sorted!");
+
+                    Thread.Sleep(500);
+
+                    Console.Clear();
+                }
+                else if (userChoice == "6" || userChoice.ToLower() == "clear list")
+                {
+                    vegetables.Clear();
+
+                    Console.WriteLine("List cleared!");
+
+                    Thread.Sleep(500);
+
+                    Console.Clear();
+                }
+                else if (userChoice == "7" || userChoice.ToLower() == "quit")
+                {
+                    Console.WriteLine("Quitting the program now...");
+
+                    Thread.Sleep(500);
+
+                    Console.Clear();
+
+                    done = true;
+                }
+                else
+                {
+                    Console.WriteLine("That's not an option, please enter a valid option");
 
                     Console.WriteLine();
 
